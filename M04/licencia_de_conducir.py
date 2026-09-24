00bc25cdb95aee6ef9b0b0b0b59de74be02283f2
sary-ilmv <glopezorozco@weber.mail.edu>
2026-09-24 00:40:31 -0600

    update

--- M04/licencia_de_conducir.py
+++ M04/licencia_de_conducir.py
@@ -26,7 +26,7 @@ paso_examen = input("Aprobaste el examen de conducir? (si/no): ").strip().lower(
 # 3. Estructura condicional
 # Primera sentencia: Debe ser mayor o igual a 18 años y haber pasado el examen
 if edad >= 18 and paso_examen == "si":
-    print("Felicidades! Eres elegible para tramitar y recibir tu licencia de conducir.")
+    print("Felicidades! Eres elegible para obtener tu licencia de conducir.")
 
 # Segunda sentencia: Es mayor de edad pero NO aprobó el examen
 elif edad >= 18 and paso_examen != "si":
@@ -34,4 +34,42 @@ elif edad >= 18 and paso_examen != "si":
 
 # Tercera sentencia: Es menor de edad
 else:
-    print("Lo sentimos, debes tener al menos 18 años para solicitar la licencia de conducir.")
+    print("Lo sentimos, debes tener 18 años para solicitar la licencia de conducir.")
+
+
+# respuesta 1: hice 2 commits en total mientras iba haciendo la tarea
+
+#respuesta 2: a mi me gusto mas usar los comandos en la terminal, 
+# al principoo da un poquito de miedo pero cuando le agarras el modo a
+#  escribir git add o git comit y git push, sientes que tienes mas control 
+# de lo que estas haciendo 
+
+#respuesta 3: sirve para revisar como esta todo antes de moverlo, tambien
+#  te avisa que archivos cambiastes y cuales todavia no se han aguerdado 
+# para que no se te olvide nada antes de hacer el commit
+
+#respuesta 4: porque si la maestra sube algo nuevo o cmbia alguna instroccion, 
+# necesitamos tener eso actualizado en nuestra laptop, si no lo hacemos, se puede 
+# cruzar las cosas y borrar alguna tare por accidente 
+
+#respuesta 5: fork es como sacarle copia al proyecto de alguine para tenerlo guardado 
+# en tu perfil de github y clone es descargar esa copia desde github a tu laptop para 
+# poder abrirla en vc code y poder editarla
+
+# respuesta 6: porque si pones nombres claros es facil saber que hicistes en cada paso, 
+# pero si solo pones cambios luego ya ni te acuerdas de que era o que es lo que hay ahi  
+
+# respuesta 7: "Agregando el codigo para la tarea de licencia de conducir" 
+# "Agregando la condicion de los lentes y la sentencia else"
+
+#respuesta 8: me guto mucho la setence 
+# if edad >= 18 and paso_examen == "si" and trae_lentes == "si": 
+# porque usa tres condiciones juntas con el operador and para asegurarse 
+# de que todo este en orden 
+
+#respuesta 9: entra cuando se cumple que es mayor de edad, pero no cumple la 
+# segunda condición que pide el programa para dejarlo manejar
+
+# respuesta 10: aprendi como hacer la tarea del modulo y usar los comentarios 
+# con # en python para explicar paso a paso lo que hace mi codigo, ademas de repasar 
+# los comandos de git para subir las entregas 
