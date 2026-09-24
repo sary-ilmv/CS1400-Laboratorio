df8aed87ef2e338e29257822896beee69fc7dbbe
sary-ilmv <glopezorozco@weber.mail.edu>
2026-09-23 20:08:44 -0600

    Agregando el codigo para la tarea de licencia de conducir

--- M04/licencia_de_conducir.py
+++ M04/licencia_de_conducir.py
@@ -15,3 +15,23 @@ Salida clara: Muestra un mensaje personalizado indicando si la persona puede
    ¿Qué imprudencia o descuido no le permitirías a tu abuela antes de subirse al auto?
      (Ejemplo: "¿Olvidó los lentes en la cocina?")
 """
+
+# 1. Pedir la edad al usuario
+edad_texto = input("Cual es tu edad?: ")
+edad = int(edad_texto)
+
+# 2. Pedir si aprobó el examen de manejo
+paso_examen = input("Aprobaste el examen de conducir? (si/no): ").strip().lower()
+
+# 3. Estructura condicional
+# Primera sentencia: Debe ser mayor o igual a 18 años y haber pasado el examen
+if edad >= 18 and paso_examen == "si":
+    print("Felicidades! Eres elegible para tramitar y recibir tu licencia de conducir.")
+
+# Segunda sentencia: Es mayor de edad pero NO aprobó el examen
+elif edad >= 18 and paso_examen != "si":
+    print("Tienes la edad suficiente, pero debes aprobar el examen práctico/teórico para obtener la licencia.")
+
+# Tercera sentencia: Es menor de edad
+else:
+    print("Lo sentimos, debes tener al menos 18 años para solicitar la licencia de conducir.")
